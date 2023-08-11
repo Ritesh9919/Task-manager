29ca1a1706ce1cb95fffc779ba218fa907a4ed19
Ritesh Maurya <riteshkumar411552@gmail.com>
2023-08-11 12:57:47 +0530

    Created router and controller for delete a task

--- controllers/task.js
+++ controllers/task.js
@@ -1,18 +1,17 @@
+const { error } = require('console');
 const Tasks = require('../models/task');
 
 module.exports.getAllTask = async (req, res) =>{
     try {
         const tasks = await Tasks.find({});
         return res.status(200).json({
-            success:true,
             data:{
                 tasks:tasks
             }
         })
     } catch (error) {
-        console.log(error);
         return res.status(500).json({
-            msg:'Internal Server Error'
+            msg:error
         })
     }
    
@@ -28,13 +27,11 @@ module.exports.createTask = async (req, res) => {
         completed:completed
     });
     return res.status(201).json({
-        success:true,
         data:task
     })
 }catch(err) {
-    console.log(err);
     return res.status(500).json({
-        msg:'Internal Server Error'
+        msg:error
     })
 }
 
@@ -45,16 +42,19 @@ module.exports.getTask = async (req, res) =>{
     try {
         const {id} = req.params;
         const task = await Tasks.findById(id);
+        if(!task) {
+            return res.status(404).json({
+                msg:`no task with this ${id}`
+            })
+        }
         return res.status(200).json({
-            success:true,
             data:{
                 task:task
             }
         })
     } catch (error) {
-        console.log(error);
-        return res.status(5000).json({
-            msg:'Internal Server Error'
+        return res.status(500).json({
+            msg:error
         })
     }
 }
@@ -68,10 +68,23 @@ module.exports.updateTask = (req, res) => {
 }
 
 
-module.exports.deleteTask = (req, res) => {
-    return res.json({
-        success:true,
-        message:'deleted'
-    })
+module.exports.deleteTask = async (req, res) => {
+    try {
+        const {id} = req.params;
+        const task = await Tasks.findOneAndDelete({_id:id});
+        if(!task) {
+            return res.status(404).json({
+                msg:`no task with this ${id}`
+            })
+        }
+
+        return res.status(200).json({
+            data:task
+        })
+    } catch (error) {
+        return res.status(500).json({
+            msg:error
+        })
+    }
 }
 
